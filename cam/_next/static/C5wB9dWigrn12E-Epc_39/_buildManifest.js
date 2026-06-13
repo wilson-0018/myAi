bcbb1c98984289c90d03801aa8fa5ec36056e5f4
weisen <wilson0018@163.com>
2026-06-13 23:03:43 +0800

    Deploy CAM insight platform to cam path

---
+++ cam/_next/static/C5wB9dWigrn12E-Epc_39/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/myAi/cam//_next/:path+",
+        "destination": "/myAi/cam/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
